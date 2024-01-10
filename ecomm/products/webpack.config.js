697875a28fc4da28963117c7dd6bc2e155631664
kartik Attri <32263277+attrikartik@users.noreply.github.com>
2024-01-09 23:50:28 -0500

    adding module federation plugin to exposed and fetch code from remote to host and adding comments

--- ecomm/products/webpack.config.js
+++ ecomm/products/webpack.config.js
@@ -1,12 +1,25 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+//plugin used to explose modules to container or other projects
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
 module.exports={
     mode:'development',
     devServer:{
         port:8081,
     },
     plugins:[
+        // setup plugin to expose files to host project
+        new ModuleFederationPlugin({
+            name:'products',
+            filename:'remoteEntry.js',
+            // expose defines which files or modules to be exposes to other projects
+            exposes:{
+                './ProductsIndex':'./src/index'
+            }
+        }),
         new HtmlWebpackPlugin({
-            template:'./public/index.html'
+            template:'./public/index.html' // render output of webpack to browser
         })
     ]
 }
